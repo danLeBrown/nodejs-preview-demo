58aad77e40b431f889959594335fb9e8c0fb37c0
Ayomide Ojo <ayomidedaniel00@gmail.com>
2026-02-07 23:03:03 +0100

    chore: some cleanup & set startup_command

--- src/db.js
+++ src/db.js
@@ -5,15 +5,10 @@ let defaultDb = null;
 
 async function connect() {
   const uri = process.env.DATABASE_URL || 'mongodb://mongodb:27017/preview-demo';
-  client = new MongoClient(uri);
-  console.log('uri', uri);
-  // console.log('client', client); 
-  
+  client = new MongoClient(uri);  
   await client.connect();
   const dbName = new URL(uri).pathname.slice(1) || 'preview-demo';
-  console.log('dbName', dbName);
   defaultDb = client.db(dbName);
-  // console.log('defaultDb', defaultDb);
 
   return client;
 }
